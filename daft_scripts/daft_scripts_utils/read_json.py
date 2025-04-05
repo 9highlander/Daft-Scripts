5c76925ae75062123d1b213172d8ce82953861fa
9highlander <154443183+9highlander@users.noreply.github.com>
2025-04-05 16:43:34 +0200

    2025-04-05 16:43

--- daft_scripts/daft_scripts_utils/read_json.py
+++ daft_scripts/daft_scripts_utils/read_json.py
@@ -4,7 +4,7 @@ import json
 def read_json(subfolder, file_name, variable_name):
     current_directory = os.getcwd()
     print("current directory",current_directory)
-    file_path = current_directory + '\\' + subfolder + '\\' + file_name
+    file_path = current_directory + '\\' + "daft_scripts" + '\\' + subfolder + '\\' + file_name
     print("file path",file_path)
     if file_path:
         with open(file_path, 'r') as file:
